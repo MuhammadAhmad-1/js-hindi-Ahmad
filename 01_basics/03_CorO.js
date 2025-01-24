e5af9d34a7a49fa69cbdadc4e2d9e6f1be2f5ec6
MuhammadAhmad-1 <161146898+MuhammadAhmad-1@users.noreply.github.com>
2025-01-24 16:20:25 +0000

    conversion of datatypes

---
+++ 01_basics/03_CorO.js
@@ -0,0 +1,25 @@
+// conversionoroperation 
+let score = 33
+let scores = "33abc"
+console.log(typeof score);
+console.log(typeof scores);
+console.log(typeof (score));
+console.log(typeof (scores));
+
+let valueInNumber = Number (scores)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+// "33" => 33
+// "33abc" => NaN
+//true => 1; false => 0 
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true; 0 => false
+// "" => false 
+// "Ahmad" => true 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
